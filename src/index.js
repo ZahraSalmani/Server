ef8cd3e0b13b0b801e2b67ba11b9a55f06aefe90
Zarsa <salmani_zahra_74@yahoo.com>
2020-10-29 11:01:09 +0330

    fix post data

--- src/index.js
+++ src/index.js
@@ -7,30 +7,40 @@ const users = require('./users.json');
 const server = http.createServer(function (req, res) {
   let file
 
-  if(req.url.indexOf('.css') != -1){ //req.url has the pathname, check if it conatins '.css'
+  if (req.url.indexOf('.css') != -1) { //req.url has the pathname, check if it conatins '.css'
 
-      fs.readFile('./css/styles.css', function (err, data) {
-        if (err) console.log(err);
-        res.writeHead(200, {'Content-Type': 'text/css'});
-        res.write(data);
-        res.end();
-      });
+    fs.readFile('./css/styles.css', function (err, data) {
+      if (err) console.log(err);
+      res.writeHead(200, { 'Content-Type': 'text/css' });
+      res.write(data);
+      res.end();
+    });
 
-    } 
-else{
-  fs.readFile('./dist/index.html', function (err, data) {
-    if (err) console.log(err);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
-  });
+  }
+  else if (req.url.indexOf('.json') != -1) { //req.url has the pathname, check if it conatins '.json'
 
-}
+    fs.readFile('./src/users.json', function (err, data) {
+      if (err) console.log(err);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.write(data);
+      res.end();
+    });
 
-  /*******GETS THE FORM DATA************/
+  }
+  else {
+    fs.readFile('./dist/index.html', function (err, data) {
+      if (err) console.log(err);
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write(data);
+      res.end();
+    });
+
+  }
   req.on('data', (data) => {
-    var arr = decodeURIComponent(data).replace(/\+/g, ' ').replace('firstname=', '')
-      .replace('lastname=', '').split('&');
+
+    var arr = decodeURIComponent(data)
+     .replace('\"', '').replace('\"', '').split(' ');
+      console.log("arr" +arr);
 
     let user = { fname: arr[0], lname: arr[1] };
 
